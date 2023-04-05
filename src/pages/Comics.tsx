58bd15d1301c6a46da38009f0f791b17570b9fbc
Vanesa Hernandez <vanesaherc@gmail.com>
2023-04-04 19:35:55 -0500

    Create page list comics

--- src/pages/Comics.tsx
+++ src/pages/Comics.tsx
@@ -1,3 +1,35 @@
+import { useState } from "react";
+import { Grid, Header } from "semantic-ui-react";
+import Container from "../components/container/Container";
+import ListComics from "../components/listComics/ListComics";
+import useFetch from "../hooks/useFetch";
+
 export default function Comics() {
-  return <div>Estamos en comics</div>;
+  const [renderComics, setRenderComics] = useState(10);
+
+  const listComics = useFetch(
+    `${import.meta.env.VITE_APP_URL_BASE}/comics?ts=1&apikey=${
+      import.meta.env.VITE_APP_API_KEY_MARVEL
+    }&hash=${
+      import.meta.env.VITE_APP_HASH_ID
+    }&orderBy=focDate&limit=${renderComics}`
+  );
+
+  return (
+    <div className="comics-page">
+      <div id="slide-comics-image" />
+      <Grid>
+        <Grid.Column>
+          <Container bg="light">
+            <Header as="h2">The best comics</Header>
+            <ListComics
+              listComics={listComics}
+              renderComics={renderComics}
+              setRenderComics={setRenderComics}
+            />
+          </Container>
+        </Grid.Column>
+      </Grid>
+    </div>
+  );
 }
